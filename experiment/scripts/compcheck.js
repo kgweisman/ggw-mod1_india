16720329c98f46a34df13164453c53f72256b537
kgweisman <kweisman@stanford.edu>
2015-04-17 11:06:24 -0700

    add stricter criteria for rejecting descriptions

--- experiment/scripts/compcheck.js
+++ experiment/scripts/compcheck.js
@@ -4,9 +4,15 @@
 
 // set up button behavior
 $('.slide#surveys button').click(function() {
+	var keyCondName = surveysSlide.condition.condName;
+	var keyWording = surveysSlide.condition.wording;
 
 	if ($('textarea#comprehensionCheck').val() == "") {
 	window.alert("Please write a description before continuing.");
+	} else if ($('textarea#comprehensionCheck').val().toLowerCase().indexOf(keyCondName.toLowerCase()) > -1) {
+	window.alert("Please use your own words to describe what this means. Do not use the word '"+keyCondName.toLowerCase()+"'.");
+	} else if ($('textarea#comprehensionCheck').val().toLowerCase().indexOf(keyWording.toLowerCase()) > -1) {
+	window.alert("Please use your own words to describe what this means. Do not use the words '"+keyWording.toLowerCase()+"'.");
 	} else {
 		// record data
 		experiment.newData.comprehensionCheck = $('textarea#comprehensionCheck').val();
