72f53d443a4474a82924186c50d571d3f28af190
kgweisman <kweisman@stanford.edu>
2015-04-18 21:38:32 -0700

    start changing compcheck to multiple choice

--- experiment/scripts/compcheck.js
+++ experiment/scripts/compcheck.js
@@ -2,20 +2,51 @@
 	
 // display question: happens in conditions.js
 
+// set up attempts counter
+var attempts = [];
+
 // set up button behavior
 $('.slide#surveys button').click(function() {
-	var keyCondName = surveysSlide.condition.condName;
-	var keyWording = surveysSlide.condition.wording;
 
-	if ($('textarea#comprehensionCheck').val() == "") {
-	window.alert("Please write a description before continuing.");
-	} else if ($('textarea#comprehensionCheck').val().toLowerCase().indexOf(keyCondName.toLowerCase()) > -1) {
-	window.alert("Please use your own words to describe what this means. Do not use the word '"+keyCondName.toLowerCase()+"'.");
-	} else if ($('textarea#comprehensionCheck').val().toLowerCase().indexOf(keyWording.toLowerCase()) > -1) {
-	window.alert("Please use your own words to describe what this means. Do not use the words '"+keyWording.toLowerCase()+"'.");
+	// store condition
+	var keyCondName;
+	if (surveysSlide.condition.condName === "Emotion Recognition") {
+		keyCondName = "emotionRecognition";
+	} else if (surveysSlide.condition.condName === "Self-Control") {
+		keyCondName = "selfControl";
 	} else {
+		keyCondName = surveysSlide.condition.condName.toLowerCase();
+	}
+
+	// store answer
+	var answer = $('input[name=compCheck]:checked', '#comp-check-form').val();
+
+	if (answer === undefined) {
+
+		// request answer
+		window.alert ("Please select the best answer from the options provided.");			
+
+	} else if (answer !== keyCondName) {
+
+		// record attempt
+		attempts.push(answer);
+
+		if (attempts.length >= 3) {
+			// give sterner warning
+			window.alert("That is not the correct answer. You need to find the correct answer before you can continue this survey. If you are unable to find the correct answer, please return this HIT.");
+
+		} else {
+			// give another try
+			window.alert ("That is not the correct answer. Please try again.");				
+		}
+
+	} else {
+
+		// record attempt
+		attempts.push(answer);
+
 		// record data
-		experiment.newData.comprehensionCheck = $('textarea#comprehensionCheck').val();
+		experiment.newData.comprehensionCheck = attempts;
 
 		// set parameters of this session
 		experiment.newData.condition = surveysSlide.condition.condName;
